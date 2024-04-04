7d5be468ada1a18962b2dba6ffcfb0f635e68eaf
b.zakaria <zakaria_bouzangad@gmail.Com>
2024-04-04 14:16:44 +0000

    working on slider arrows

--- assets/swiper-slider.js
+++ assets/swiper-slider.js
@@ -1,34 +1,35 @@
 class SwiperComponent extends HTMLElement {
-  constructor() {
-    super();
+    constructor() {
+      super();
+    }
+  
+    connectedCallback() {
+      this.initSwiper();
+  
+      // Listen for window resize events
+      window.addEventListener("resize", () => this.handleResize());
+    }
+  
+    initSwiper() {
+      // Configure Swiper with the retrieved options
+      debugger
+      const swiperOptions = {
+        slidesPerView: 2.7,
+        spaceBetween: 30,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      };
+  
+      // Check screen width and initialize Swiper accordingly
+      new Swiper(this, swiperOptions)
+    }
+  
+    handleResize() {
+      this.initSwiper();
+    }
   }
-
-  connectedCallback() {
-    this.initSwiper();
-
-    // Listen for window resize events
-    window.addEventListener("resize", () => this.handleResize());
-  }
-
-  initSwiper() {
-    // Configure Swiper with the retrieved options
-    debugger
-    const swiperOptions = {
-      slidesPerView: 2.5,
-      spaceBetween: 30,
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    };
-
-    // Check screen width and initialize Swiper accordingly
-    new Swiper(this, swiperOptions)
-  }
-
-  handleResize() {
-    this.initSwiper();
-  }
-}
-
-customElements.define("swiper-component", SwiperComponent);
+  
+  customElements.define("swiper-component", SwiperComponent);
+  
---
+++ dev/js/swiper-slider.js
@@ -0,0 +1,35 @@
+class SwiperComponent extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+    connectedCallback() {
+      this.initSwiper();
+  
+      // Listen for window resize events
+      window.addEventListener("resize", () => this.handleResize());
+    }
+  
+    initSwiper() {
+      // Configure Swiper with the retrieved options
+      debugger
+      const swiperOptions = {
+        slidesPerView: 2.7,
+        spaceBetween: 30,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      };
+  
+      // Check screen width and initialize Swiper accordingly
+      new Swiper(this, swiperOptions)
+    }
+  
+    handleResize() {
+      this.initSwiper();
+    }
+  }
+  
+  customElements.define("swiper-component", SwiperComponent);
+  
