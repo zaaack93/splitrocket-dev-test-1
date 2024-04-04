0279d013eec2aca62534e6096481a0f09e66fbb9
bouzangad zakaria <bouzangadzakaria@gmail.com>
2024-04-04 17:52:50 +0000

    working on swiper slide responsive

--- assets/swiper-slider.js
+++ assets/swiper-slider.js
@@ -1,35 +1,35 @@
 class SwiperComponent extends HTMLElement {
     constructor() {
       super();
-    this.dataColumnsDesktop = this.getAttribute("data-columns-desktop");
-    this.dataColumnsMobile = this.getAttribute("data-columns-mobile");
     }
   
     connectedCallback() {
       this.initSwiper();
-  
-      // Listen for window resize events
-      window.addEventListener("resize", () => this.handleResize());
     }
   
     initSwiper() {
       // Configure Swiper with the retrieved options
-      debugger
       const swiperOptions = {
-        slidesPerView: this.dataColumnsDesktop,
-        spaceBetween: 30,
+        slidesPerView: 1,
+        spaceBetween: 20,
+        breakpoints: {
+          1024: {
+            slidesPerView: 2.55,
+            spaceBetween: 30,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          }
+        },
         navigation: {
           nextEl: ".swiper-button-next",
           prevEl: ".swiper-button-prev",
-        },
+        }
       };
   
       // Check screen width and initialize Swiper accordingly
       new Swiper(this, swiperOptions)
-    }
-  
-    handleResize() {
-      this.initSwiper();
     }
   }
   
