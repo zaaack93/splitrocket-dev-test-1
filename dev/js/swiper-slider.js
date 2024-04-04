a45012e45556547ccb8060943dda6836050dcb6d
bouzangad zakaria <bouzangadzakaria@gmail.com>
2024-04-04 18:23:26 +0000

    Optimize and comment the code

--- dev/js/swiper-slider.js
+++ dev/js/swiper-slider.js
@@ -1,37 +1,41 @@
+// Define a class for the Swiper component that extends the HTMLElement class
 class SwiperComponent extends HTMLElement {
-    constructor() {
-      super();
-    }
-  
-    connectedCallback() {
-      this.initSwiper();
-    }
-  
-    initSwiper() {
-      // Configure Swiper with the retrieved options
-      const swiperOptions = {
-        slidesPerView: 1,
-        spaceBetween: 20,
-        breakpoints: {
-          1024: {
-            slidesPerView: 2.55,
-            spaceBetween: 30,
-          },
-          768: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-          }
-        },
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        }
-      };
-  
-      // Check screen width and initialize Swiper accordingly
-      new Swiper(this, swiperOptions)
-    }
+  // The constructor is called when an object is created with the class
+  constructor() {
+    super(); // Call the parent class constructor
   }
-  
-  customElements.define("swiper-component", SwiperComponent);
-  
+
+  // connectedCallback is a lifecycle method that is called when the custom element is first connected to the document's DOM
+  connectedCallback() {
+    this.initSwiper(); // Initialize the Swiper when the component is connected
+  }
+
+  // Method to initialize the Swiper
+  initSwiper() {
+    // Define the options for the Swiper
+    const swiperOptions = {
+      slidesPerView: 1, // Number of slides per view
+      spaceBetween: 20, // Space between slides
+      breakpoints: { // Breakpoints for different screen widths
+        1024: {
+          slidesPerView: 2.55,
+          spaceBetween: 30,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        }
+      },
+      navigation: { // Navigation options
+        nextEl: ".swiper-button-next", // Selector for the next button
+        prevEl: ".swiper-button-prev", // Selector for the previous button
+      }
+    };
+
+    // Initialize the Swiper with the options
+    new Swiper(this, swiperOptions);
+  }
+}
+
+// Define the custom element
+customElements.define("swiper-component", SwiperComponent);
